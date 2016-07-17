f60f47a47e5bc3367a2c1f9afe4f39c7114d7c19
Steve Duncan <SteveDuncan@comcast.net>
2016-07-16 17:11:57 -0700

    Add Ajax to Flask AjaxPosts

--- AjaxPosts/server.py
+++ AjaxPosts/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, Markup
+from flask import Flask, render_template, request, redirect, jsonify
 from mysqlconnection import MySQLConnector
 
 app=Flask(__name__)
@@ -7,8 +7,8 @@ mysql=MySQLConnector(app,'myownapi')
 
 @app.route('/')
 def index():
-	select_query = "SELECT * FROM posts"
-	posts = mysql.query_db(select_query)
+	query = "SELECT * FROM posts"
+	posts = mysql.query_db(query)
 	
 	return render_template('index.html',posts=posts)
 
@@ -17,11 +17,24 @@ def index():
 def create():
 	query = "INSERT INTO posts (description) VALUES('{}')".format(request.form['newpost'])
 	mysql.query_db(query)
-	
-	select_query = "SELECT * FROM posts"
-	posts = mysql.query_db(select_query)
 
-	# return redirect('/')
-	return render_template('index.html',posts=posts)
+	return redirect('/')
+
+
+@app.route('/create_json',methods=['GET', 'POST'])
+def create_json():
+	query = "INSERT INTO posts (description) VALUES('{}')".format(request.form['newpost'])
+	mysql.query_db(query)
+
+	query = "SELECT * FROM posts"
+	posts = mysql.query_db(query)
+	return jsonify(posts)
+
+
+@app.route('/index_html',methods=['POST'])
+def index_partial():
+    query = "SELECT * FROM posts"
+    posts = mysql.query_db(query)
+    return render_template('posts.html', posts=posts)
 
 app.run(debug=True)
